f47766f82804602c5d7d1b3bd6df16e2da29dd94
Sarah <sarahrudy@gmail.com>
2022-03-17 20:10:35 -0600

    Add OpenSans fonts and weights

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import location from '../../assets/location.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="location-and-city-state">
+        <img src={location} alt='location indicator'></img>
+        <h1>Dallas, TX</h1>
+      </div>
+      <h2></h2>
     </div>
   );
 }
