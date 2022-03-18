dced39e47c47e4c6e1c6bd1c9b861803da17066c
Sarah <sarahrudy@gmail.com>
2022-03-17 22:04:46 -0600

    Create Forecast component

---
+++ src/components/Forecast/Forecast.js
@@ -0,0 +1,20 @@
+import './Forecast.css' 
+import cloudDrizzleSun from '../../assets/cloud-drizzle-sun.png'
+import blackCircle from '../../assets/black-circle.png'
+import ForecastCard from '../ForecastCard/ForecastCard'
+
+// get APIcall working first, then come back to this 
+
+const Forecast = ({ forecast }) => {
+  // const forecastCard = forecast.map(( forecast, i) => {
+    return (
+     <ForecastCard />
+    )
+  // })
+
+  // return (
+  //   <div className="forecast-container">{ forecastCard }</div>
+  // )
+}
+
+export default Forecast 
