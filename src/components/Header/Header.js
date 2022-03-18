e00e82a2ac7621c5aef197765d4245d11411d0da
Sarah <sarahrudy@gmail.com>
2022-03-17 21:24:25 -0600

    Import dayjs and format date

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,12 +7,12 @@ const Header = () => {
   const today = dayjs(new Date())
 
   return (
-    <div className="App">
+    <div className="Header">
       <div className="location-and-city-state">
         <img src={location} alt='location indicator'></img>
         <h1>Dallas, TX</h1>
       </div>
-      <h2></h2>
+      <h2 className="date">{ today.format('dddd, MMMM DD, YYYY') }</h2>
     </div>
   )
 }
