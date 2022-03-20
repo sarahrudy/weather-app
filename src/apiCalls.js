e15a6fe9760226aa2749b8feb72ac632a1592266
Sarah <sarahrudy@gmail.com>
2022-03-20 15:12:50 -0600

    Fix fetch for One Call from API

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,27 @@
-export const getData = (id) => {
-  return fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=7b939d7f115eb5ec22876af777e193e9&units=imperial`)
-    .then(response => response.json())
-    .then(data => console.log(data))
+// getWeather with ID
+// export const getWeatherData = (id) => {
+//   return fetch(`https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=555ea941e4367c7fd6388885aac3c9fd&units=imperial`)
+//   .then(response => checkError(response))
+// }
+
+// getWeather hardcoded 
+// export const getWeatherData = () => {
+//   return fetch(`https://api.openweathermap.org/data/2.5/weather?id=4684888&appid=555ea941e4367c7fd6388885aac3c9fd&units=imperial`)
+//   .then(response => checkError(response))
+// }
+
+// getForecast 
+// export const getForecast = () => {
+//   return fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=555ea941e4367c7fd6388885aac3c9fd&units=imperial`)
+//   .then(response => checkError(response))
+// }
+
+// oneCall 
+export const oneCall = () => {
+  return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=32.783058&lon=-96.806671&exclude=minutely&appid=555ea941e4367c7fd6388885aac3c9fd&units=imperial`)
+  .then(response => checkError(response))
 }
+
 
 export const checkError = (response) => {
   if (!response.ok) {
