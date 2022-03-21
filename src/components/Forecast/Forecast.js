9e98d30914dcd1c6864d19146b3622d86ff872aa
Sarah <sarahrudy@gmail.com>
2022-03-20 22:19:00 -0600

    Convert dt to days of the week and display them

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,5 +1,4 @@
 import './Forecast.css' 
-import cloudDrizzleSun from '../../assets/cloud-drizzle-sun.png'
 import ForecastCard from '../ForecastCard/ForecastCard'
 
 const Forecast = ({ weather }) => {
@@ -8,15 +7,27 @@ const Forecast = ({ weather }) => {
     return <h2>is loading...</h2>
   }
 
-  // const forecastCard = forecast.daily.map(( forecast, i) => {
-    return (
-     <ForecastCard />
-    )
-  // })
+  const forecastCard = weather.daily.map(( forecast, i) => {
 
-  // return (
-  //   <div className="forecast-container">{ forecastCard }</div>
-  // )
+    const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const d = new Date(forecast.dt * 1000)
+    let day = weekday[d.getDay()].slice(0, 3)
+
+    return (
+      <div className="forecast-container">
+        <ForecastCard 
+          key={ i }
+          day={ day }
+          weatherCondition={ forecast.weather[0].main }
+          temperature={ Math.round(forecast.temp.day) }
+        />
+      </div>
+    )
+  })
+
+  return (
+    <div className="forecast-container">{ forecastCard }</div>
+  )
 }
 
 export default Forecast 
