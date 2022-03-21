6b212e9428e7206f6ed3af5d8703783d2dfecefc
Sarah <sarahrudy@gmail.com>
2022-03-20 22:39:10 -0600

    Move key to get rid of key prop error

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -7,16 +7,15 @@ const Forecast = ({ weather }) => {
     return <h2>is loading...</h2>
   }
 
-  const forecastCard = weather.daily.map(( forecast, i) => {
+  const forecastCard = weather.daily.map(( forecast, index) => {
 
     const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     const d = new Date(forecast.dt * 1000)
     let day = weekday[d.getDay()].slice(0, 3)
 
     return (
-      <div className="forecast-container">
+      <div className="forecast-container" key={ index }>
         <ForecastCard 
-          key={ i }
           day={ day }
           weatherCondition={ forecast.weather[0].main }
           temperature={ Math.round(forecast.temp.day) }
